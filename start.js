9e4847c0d1c177b6ac7819364211ea0d03d522c4
stoneCC <yf1983321@sina.com>
2015-04-23 06:31:45 +0800

    delete file

--- start.js
+++
@@ -1,7 +0,0 @@
-var child_process = require('child_process');
-child_process.exec('node server.js', function(err, stdout, stderr) {
-  console.log(stdout);
-});
-child_process.exec('node bin/proxy.js', function(err, stdout, stderr) {
-  console.log(stdout);
-});
--- start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-nohup supervisor -w conf -e json bin/proxy.js &
-nohup supervisor -w conf -e json server.js &
